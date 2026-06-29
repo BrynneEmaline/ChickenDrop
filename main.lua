1cbd48701f980ba96db78bcf588f0304eb5951fb
Brynne Alexander <brynne.emaline@gmail.com>
2026-06-28 22:57:25 -0500

    finished special block randomization, game over screen

--- main.lua
+++ main.lua
@@ -19,17 +19,26 @@ function love.load()
     fallRate = {love.math.random(50, 200), love.math.random(50, 200), love.math.random(50, 200),
                 love.math.random(50, 200), love.math.random(50, 200)}
 
-    specialStartX = love.math.random(love.graphics.getWidth() - specialBrick:getWidth())
-    specialStartY = math.random(love.graphics.getHeight() - specialBrick:getHeight())
-
     score = 0
     gameOver = false
+
     upgradeOn = false
     upgradeTimer = 0
-    timer = 0
-    timeToSpecialBrick = love.math.random(1, 2)
-    --respawnSpecialTimer = love.math.random(1, 2)
 
+    specialOn = false
+    specialTimer = 0
+    specialDelay = love.math.random(4, 8)
+
+    specialStartX = 0
+    specialStartY = 0
+
+end
+
+function spawnSpecial(dt)
+    specialOn = true
+
+    specialStartX = love.math.random(love.graphics.getWidth() - specialBrick:getWidth())
+    specialStartY = math.random(love.graphics.getHeight() - specialBrick:getHeight())
 end
 
 function love.mousepressed(x, y, button, istouch) -- istouch is a boolean
@@ -45,24 +54,26 @@ function love.mousepressed(x, y, button, istouch) -- istouch is a boolean
                 break
             end
         end
-        if x >= specialStartX and x <= specialStartX + specialBrick:getWidth() and y >= specialStartY and y <=
-            specialStartY + specialBrick:getHeight() then
+        if specialOn and x >= specialStartX and x <= specialStartX + specialBrick:getWidth() and y >= specialStartY and
+            y <= specialStartY + specialBrick:getHeight() then
 
             upgradeOn = true
-            specialStartX = love.math.random(love.graphics.getWidth() - specialBrick:getWidth())
-            specialStartY = math.random(love.graphics.getHeight() - specialBrick:getHeight())
+            specialOn = false
+            SpecialTimer = 0
+            SpecialDelay = love.math.random(4, 8)
 
         end
     end
 end
 
 function love.update(dt) -- delta time as parameter
-    timer = timer + dt
+    -- upgradeTimer = upgradeTimer + dt
+    -- specialTimer = specialTimer + dt
+
     fallRateUpgrade = 20
 
-    -- handles how long the upgrade lasts
+    -- handles the fall rate when active
     if upgradeOn then
-        upgradeTimer = upgradeTimer + dt
         for i, value in ipairs(startX) do
             startY[i] = startY[i] + fallRateUpgrade * dt;
         end
@@ -79,46 +90,54 @@ function love.update(dt) -- delta time as parameter
     end
 
     -- handles ending the upgrade, resets timers
-    if upgradeTimer >= 5 then -- ends the upgrade after 5 seconds
-        upgradeTimer = 0
-        upgradeOn = false
-        timer = 0
-        timeToSpecialBrick = love.math.random(1, 2)
-    end
+    if upgradeOn then
+        upgradeTimer = upgradeTimer + dt
 
-    --[[ if specialOnScreen then
-        specialOnScreenTimer = specialOnScreenTimer + dt
-        if specialOnScreenTimer >= 5 then
-            timer = 0
-            timeToSpecialBrick = love.math.random(1, 2)
-            specialOnScreen = false
+        if upgradeTimer >= 2 then -- ends the upgrade after 2 seconds
+            upgradeTimer = 0
+            upgradeOn = false
         end
     end
 
-    if not upgradeOn and timer >= timeToSpecialBrick then
-        specialOnScreen = true
-        specialOnScreenTimer = 0
+    if not specialOn then
+        specialTimer = specialTimer + dt
+
+        if specialTimer >= specialDelay then
+            spawnSpecial()
+            specialTimer = 0
+            specialDelay = love.math.random(4, 8)
+        end
     end
-]]
-
 end
-
 function love.draw()
+
+    love.graphics.setFont(love.graphics.newFont(24))
 
     if gameOver then
         love.graphics.print('GAME OVER', 500, 500)
-        love.graphics.print('Press R to restart', 500, 530)
+        love.graphics.print('SCORE: ' .. score, 500, 530)
+        love.graphics.print('Press R to restart', 500, 560)
         return
     end
 
-    love.graphics.print(score, 400, 300) -- check to see if font could be prettier
-    love.graphics.print(timer, 200, 200)
-
-    if timer > timeToSpecialBrick then
-        love.graphics.draw(specialBrick, specialStartX, specialStartY)
-    end
+    love.graphics.print('SCORE: ' .. score, love.graphics.getWidth() - 150, love.graphics.getHeight() - 50) -- check to see if font could be prettier
 
     for i, value in ipairs(startX) do
         love.graphics.draw(brickGuy, startX[i], startY[i]);
     end
+
+    if specialOn and not upgradeOn then
+        love.graphics.draw(specialBrick, specialStartX, specialStartY)
+    end
 end
+
+function restartGame()
+    love.load()
+end
+
+function love.keypressed(key)
+    if key == "r" and gameOver then
+        restartGame()
+    end
+end
+
